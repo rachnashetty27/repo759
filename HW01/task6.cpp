591ec91b01a8540dab1659117dbdee8d7ff7d324
rachnashetty <rachnashetty@rachnashettys-MacBook-Air.local>
2025-02-02 20:13:53 -0600

    Task6 committedd

---
+++ HW01/task6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+
+int main(int argc, char* argv[]) {
+    // Check if argument is provided
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " N" << std::endl;
+        return 1;
+    }
+
+    // Convert command line argument to integer
+    int N = std::atoi(argv[1]);
+
+    // Print ascending order using printf
+    for (int i = 0; i <= N; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    // Print descending order using std::cout
+    for (int i = N; i >= 0; i--) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
