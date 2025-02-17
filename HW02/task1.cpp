365e9bc014441fb8216920d252da2c8efc12ad5e
rachnashetty27 <rrshetty3@wisc.edu>
2025-02-18 00:44:46 +0530

    Updated

--- HW02/task1.cpp
+++ HW02/task1.cpp
@@ -1,4 +1,4 @@
-//This task has been done with the help of chatgpt, claude.ai and google search
+//This task has been done with the help of chatgpt, claude.ai and google search.
 #include <iostream>
 #include <vector>
 #include <cstdlib>
