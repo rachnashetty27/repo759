7693d3ef13bb679b3bab0c8072a45b8eb181a1d2
rachnashetty <rachnashetty@rachnasettysAir.attlocal.net>
2025-03-02 22:41:03 -0600

    Updated

--- HW03/matmul.cpp
+++ HW03/matmul.cpp
@@ -3,15 +3,15 @@
 
 // Function to perform parallel matrix multiplication using OpenMP
 void mmul(const float* matA, const float* matB, float* matC, const std::size_t size) {
-    #pragma omp parallel for collapse(2)
+    #pragma omp parallel for collapse(2) schedule(dynamic)
     for (std::size_t r = 0; r < size; r++) {
         for (std::size_t c = 0; c < size; c++) {
-            float tempSum = 0.0f;
+            float sum = 0.0f;
             for (std::size_t k = 0; k < size; k++) {
-                tempSum += matA[r * size + k] * matB[k * size + c];
+                sum += matA[r * size + k] * matB[k * size + c];
             }
-            #pragma omp atomic
-            matC[r * size + c] += tempSum;
+            matC[r * size + c] = sum; // Removed atomic
         }
     }
 }
+
